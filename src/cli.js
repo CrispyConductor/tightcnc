b16027319358e6eb05819f674fbfc7430200b365
Chris Breneman <crispy@cluenet.org>
2019-12-02 18:00:04 -0500

    More

---
+++ src/cli.js
@@ -0,0 +1,30 @@
+const argv = require('yargs').argv;
+const littleconf = require('littleconf');
+const SimpleCNCClient = require('./clientlib');
+
+if (!argv.operation && !argv.o) {
+	console.error('Must provide operation to execute with --operation');
+	process.exit(1);
+}
+
+let operation = argv.operation || argv.o;
+let params = {};
+for (let key in argv) {
+	if (/^[a-zA-Z]/.test(key) && key !== 'o' && key !== 'operation') {
+		let value = argv[key];
+		try { value = JSON.parse(value); } catch(err) {}
+		params[key] = value;
+	}
+}
+
+let config = littleconf.getConfig();
+let client = new SimpleCNCClient(config);
+
+client.op(operation, params)
+	.then((r) => {
+		console.log(JSON.stringify(r, null, 4));
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+
